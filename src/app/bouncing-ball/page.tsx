800a8c0b533a4d213bef7d5d3f093457278f398f
Mark <ehdgns5164@naver.com>
2025-03-12 20:58:00 +0900

    bouncing-ball ui

--- src/app/bouncing-ball/page.tsx
+++ src/app/bouncing-ball/page.tsx
@@ -1,5 +1,30 @@
+'use client';
+
 const BouncingBallPage = () => {
-  return <div></div>;
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <div style={{ position: 'relative' }}>
+        <div
+          style={{
+            position: 'absolute',
+            left: '100px',
+            width: '30px',
+            height: '30px',
+            backgroundColor: 'red',
+            borderRadius: '50%',
+          }}
+        ></div>
+        <div
+          style={{
+            position: 'absolute',
+            top: '500px',
+            width: '100%',
+            borderTop: '1px solid black',
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default BouncingBallPage;

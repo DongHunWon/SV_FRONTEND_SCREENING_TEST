9932a112060d9a5d85b41398932bc07b14aa65ad
Mark <ehdgns5164@naver.com>
2025-03-14 14:27:21 +0900

    bouncing-ball-환경 추가

--- src/app/bouncing-ball/page.tsx
+++ src/app/bouncing-ball/page.tsx
@@ -5,11 +5,11 @@ import { useEffect, useRef, useState } from 'react';
 interface Ball {
   y: number;
   vy: number;
-  ay: number;
   radius: number;
 }
 
-const INIT_BALL = { y: 0, vy: 0, ay: 0.5, radius: 15 };
+const INIT_GRAVITY_ACCELERATION = 9.81;
+const INIT_BALL = { y: 0, vy: 0, radius: 15 };
 const FLOOR_HEIGHT = 700;
 const THRESHOLD = 300;
 
@@ -17,10 +17,12 @@ const BouncingBallPage = () => {
   const ballRef = useRef<HTMLDivElement>(null);
   const [ball, setBall] = useState<Ball>(INIT_BALL);
   const [isMoved, setIsMoved] = useState<boolean>(false);
+  const [gravityAcceleration, setGravityAcceleration] = useState<number>(INIT_GRAVITY_ACCELERATION);
+  const [height, setHeight] = useState<number>(FLOOR_HEIGHT);
 
   useEffect(() => {
     setTimeout(() => {
-      const { y, vy, ay } = ball;
+      const { y, vy } = ball;
 
       if (y >= FLOOR_HEIGHT) {
         setIsMoved(false);
@@ -28,7 +30,7 @@ const BouncingBallPage = () => {
       }
 
       if (isMoved) {
-        const newVy = vy + ay;
+        const newVy = vy + gravityAcceleration;
         const newY = y + newVy > FLOOR_HEIGHT ? FLOOR_HEIGHT : y + newVy;
 
         movedBall({ ...ball, y: newY, vy: newVy });
@@ -40,6 +42,8 @@ const BouncingBallPage = () => {
     let count = 0;
     let lastPressTime = 0;
     let timer: ReturnType<typeof setTimeout> | undefined = undefined;
+
+    movedBall({ ...ball, y: FLOOR_HEIGHT - height });
 
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.code === 'Space') {
@@ -58,7 +62,7 @@ const BouncingBallPage = () => {
           if (count === 2) {
             setIsMoved(true);
           } else if (count === 3) {
-            movedBall(INIT_BALL);
+            movedBall({ ...INIT_BALL, y: FLOOR_HEIGHT - height });
           }
         }, THRESHOLD);
       }
@@ -66,7 +70,7 @@ const BouncingBallPage = () => {
 
     window.addEventListener('keypress', handleKeyDown);
     return () => window.removeEventListener('keypress', handleKeyDown);
-  }, []);
+  }, [height]);
 
   const movedBall = (ball: Ball) => {
     const ballEl = ballRef.current;
@@ -78,12 +82,23 @@ const BouncingBallPage = () => {
 
   return (
     <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+      <div style={{ position: 'absolute', top: '50px', display: 'flex', flexDirection: 'column', gap: '15px' }}>
+        <div>제약 조건 : 공기 저항 x</div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <span>중력 가속도 : {gravityAcceleration}</span>
+          <input type="range" min="0.1" max="50" value={gravityAcceleration} onChange={(e) => setGravityAcceleration(Number(e.target.value))} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <span>높이 : {height}</span>
+          <input type="range" min="0" max={FLOOR_HEIGHT} value={height} onChange={(e) => setHeight(Number(e.target.value))} />
+        </div>
+      </div>
       <div
         ref={ballRef}
         style={{
           position: 'absolute',
           left: '50%',
-          top: `${INIT_BALL.y}px`,
+          top: `${FLOOR_HEIGHT - height}}px`,
           width: `${ball.radius * 2}px`,
           height: `${ball.radius * 2}px`,
           backgroundColor: 'red',
